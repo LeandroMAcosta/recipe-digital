3563577d9e51ade823b8cc95d79bc8f91a4b18f2
Leandro Acosta <leacosta97@gmail.com>
2021-05-22 21:30:32 -0300

    Fix bug on server port configuration

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,10 @@ import { connect } from "./config/typeorm";
 async function main() {
   await connect();
   const app = await startServer();
-  app.listen(3000);
-  console.log("Start server on port ", 3000);
+  app.listen({
+    port: process.env.PORT || 3000
+  });
+  console.log("Start server on port ", process.env.PORT);
 }
 
 main();
